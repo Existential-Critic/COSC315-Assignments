fd29e78d8a91f2b1a06e83b28a1cd099ad2359d5
Soren Stenback <63319055+Existential-Critic@users.noreply.github.com>
2023-01-24 15:37:26 -0800

    Add files via upload

---
+++ A1/Q1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // 1. Get the number of inputs from the user
+
+    // 2. Read the number of inputs from the user
+    // You will need to use an array to handle all of the inputs.
+    // Basic array can be defined as such: int a[int_length]
+
+    // 3. Calculate the min, max, sum and average
+    // Make sure to cast ints to floats for proper decimal output
+
+    // 4. Correctly print the stats
+
+    return 0;
+}
---
+++ A1/Q3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int range_start = 40;
+    int range_end = 100;
+    printf("Generating numbers from %d to %d\n", range_start, range_end);
+
+    // 1. Get the number of ints to generate from the user
+
+    // 2. Malloc an int array and fill with random numbers
+    // Randomly generated numbers should fall within the defined range
+
+    // 3. Print generated numbers
+
+    // 4. Malloc a char array and fill by casting numbers from the int array
+
+    // 5. Print the random letters (HINT: use %s)
+
+    return 0;
+}
