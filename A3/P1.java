354471440133409ad75e85179404131fe0257fad
Soren Stenback <soren.is@hotmail.ca>
2023-03-10 15:40:38 -0800

    Fixed P1.java
    Adjusted to classes for Producer and Consumer and BoundedBuffer, implemented dual locks.

--- A3/P1.java
+++ A3/P1.java
@@ -3,59 +3,43 @@ import java.util.*;
 
 public class P1 {
     public static void main(String args[]) throws InterruptedException {
+        // Create BoundedBuffer of type Request
+        BoundedBuffer<Request> bufferQueue = new BoundedBuffer<Request>(5);
+
+        // Create Producer
+        Producer prod = new Producer(bufferQueue);
+
         // Create scanner to read user input
         Scanner sc = new Scanner(System.in);
         // Ask user for how many consumer threads are needed and save to variable
         System.out.println("How many consumer threads are there?");
         int numCon = sc.nextInt();
 
-        // Create queue
-        final Queue q = new Queue();
-
-        // Create producer thread
-        Thread prod = new Thread(
-            new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        q.produce();
-                    }catch(InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        );
-
-        // Create array of consumer threads
-        Thread[] con = new Thread[numCon];
-        // Fill consumer array with consumer threads
+        // Create Consumers
+        Consumer[] con = new Consumer[numCon];
         for(int i = 0;i < con.length;i++) {
-            final int conId = i;
-            con[i] = new Thread(
-                new Runnable() {
-                    int id = conId;
-                    @Override
-                    public void run() {
-                        try {
-                            q.consume(id);
-                        }catch(InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            );
+            con[i] = new Consumer(bufferQueue,i+1);
+        }
+
+        // Create Producer thread
+        Thread prodThread = new Thread(prod);
+
+        // Create Consumer threads
+        Thread[] conThreads = new Thread[numCon];
+        for(int i = 0;i < conThreads.length;i++) {
+            conThreads[i] = new Thread(con[i]);
         }
 
         // Start all threads
-        prod.start();
-        for(int i = 0;i<con.length;i++) {
-            con[i].start();
+        prodThread.start();
+        for(int i = 0;i < conThreads.length;i++) {
+            conThreads[i].start();
         }
 
-        // Finish all threads
-        prod.join();
-        for(int i = 0;i<con.length;i++) {
-            con[i].join();
+        // Join all threads
+        prodThread.join();
+        for(int i = 0;i < conThreads.length;i++) {
+            conThreads[i].join();
         }
 
         // Close scanner
@@ -75,74 +59,133 @@ class Request {
     }
 }
 
-class Queue {
+class BoundedBuffer<T> {
     // Initialise needed variables
-    LinkedList<Request> list = new LinkedList<>();
-    int capacity = 5;
-    long startTime = System.currentTimeMillis();
+    private final Object[] buffer;
+    private int prodPos,conPos,count;
+    // Initialise monitors
+    private final Object prodMonitor = new Object();
+    private final Object conMonitor = new Object();
 
-    // Producer function
-    public void produce() throws InterruptedException {
-        int id = 0;
-        int maxTime = 10;
-        int minTime = 1;
-        int range = (maxTime-minTime+1);
-        Random rand = new Random();
-        while(true) {
-            synchronized(this) {
-                // If list is at capacity, wait
-                while(list.size() == capacity) {
-                    wait();
+    // Constructor
+    public BoundedBuffer(int bound) {
+        buffer = new Object[bound];
+    }
+
+    // Produce method and helper method
+    public void produce(Request req) {
+        synchronized(prodMonitor) {
+            try {
+                while(isFull()) {
+                    prodMonitor.wait();
                 }
-                // Create request to be added
-                Request req = new Request(id,rand.nextInt(range+minTime));
-                // Increment id
-                id++;
-                // Get current time
-                long curTime = System.currentTimeMillis();
-                // Get time elapsed in seconds
-                float elapsedTime = (curTime - startTime)/1000F;
-                // Print producer message
-                System.out.printf("Producer: Produced request ID %d, length %d seconds at time %f.%n",req.reqID,req.length,elapsedTime);
-                // Add the request to the list
-                list.add(req);
-                // Notify the consumer threads they can consume
-                notify();
-                // Notify user about producer sleeping
-                System.out.println("Producer: sleeping for 5 seconds");
-                Thread.sleep(5000);
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        doProduce(req);
+        synchronized(conMonitor) {
+            conMonitor.notifyAll();
+        }
+    }
+    protected void doProduce(Request req) {
+        buffer[prodPos] = req;
+        if(++prodPos == buffer.length) {
+            prodPos = 0;
+        }
+        ++count;
+    }
+
+    // Consume method and helper method
+    public Request consume() {
+        synchronized(conMonitor) {
+            try {
+                while(isEmpty()) {
+                    conMonitor.wait();
+                }
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Request req = doConsume();
+        synchronized(prodMonitor) {
+            prodMonitor.notifyAll();
+        }
+        return req;
+    }
+    protected Request doConsume() {
+        Request req = (Request)buffer[conPos];
+        if(++conPos == buffer.length) {
+            conPos = 0;
+        }
+        --count;
+        return req;
+    }
+
+    // Additional helper methods
+    public boolean isFull() {
+        return count == buffer.length;
+    }
+    public boolean isEmpty() {
+        return count == 0;
+    }
+}
+
+class Producer implements Runnable {
+    // Initialise BoundedBuffer variable
+    BoundedBuffer<Request> queue;
+    int maxTime = 10;
+    int minTime = 1;
+    int reqId = 0;
+    // Constructor
+    public Producer(BoundedBuffer<Request> bbuff) {
+        this.queue = bbuff;
+    }
+    // Run method from Runnable
+    public void run() {
+        long startTime = System.currentTimeMillis();
+        while(true) {
+            try {
+                reqId++;
+                Random rand = new Random();
+                Request newReq = new Request(reqId,rand.nextInt(((maxTime+1)-minTime)+minTime));
+                queue.produce(newReq);
+                long currTime = System.currentTimeMillis();
+                float timeElapsed = (currTime-startTime)/1000F;
+                System.out.printf("Producer: Produced request ID %d, length %d seconds at time %f.%n", newReq.reqID,newReq.length,timeElapsed);
+                System.out.println("Sleeping for 1 second.");
+                Thread.sleep(1000);
+            }catch(InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
+}
 
-    // Consumer function
-    public void consume(int conId) throws InterruptedException {
+class Consumer implements Runnable {
+    // Initialise needed variables
+    BoundedBuffer<Request> queue;
+    int conId;
+    // Constructor
+    public Consumer(BoundedBuffer<Request> bbuff,int id) {
+        this.queue = bbuff;
+        this.conId = id;
+    }
+    // Run method from Runnable
+    public void run() {
+        long startTime = System.currentTimeMillis();
         while(true) {
-            synchronized(this) {
-                // If list is empty, wait
-                while(list.size() == 0) {
-                    wait();
-                }
-                // Retrieve first request in the list
-                Request req = list.removeFirst();
-                // Get current time
-                long curTime = System.currentTimeMillis();
-                // Get time elapsed in seconds
-                float elapsedTime = (curTime - startTime)/1000F;
-                // Print consumer message
-                System.out.printf("Consumer %d: Assigned request ID %d, processing request for the next %d seconds, current time is %f.%n",conId,req.reqID,req.length,elapsedTime);
-                // Sleep for the duration of the request
-                Thread.sleep(req.length*1000);
-                // Get current time
-                curTime = System.currentTimeMillis();
-                // Get time elapsed in seconds
-                elapsedTime = (curTime - startTime)/1000F;
-                // Print final consumer method
-                System.out.printf("Consumer %d: Completed request ID %d at time %f.%n",conId,req.reqID,elapsedTime);
-                // Wake the producer thread
-                notify();
-                // Sleep consumer thread
-                Thread.sleep(30000);
+            try {
+                Request conReq = queue.consume();
+                long currTime = System.currentTimeMillis();
+                float timeElapsed = (currTime-startTime)/1000F;
+                System.out.printf("Consumer %d: Assigned request ID %d, processing request for the next %d seconds, current time is %f.%n",conId,conReq.reqID,conReq.length,timeElapsed);
+                Thread.sleep(conReq.length*1000);
+                currTime = System.currentTimeMillis();
+                timeElapsed = (currTime-startTime)/1000F;
+                System.out.printf("Consumer %d: Completed request ID %d at time %f.%n",conId,conReq.reqID,timeElapsed);
+            }catch(InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
